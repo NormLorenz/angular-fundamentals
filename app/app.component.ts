498b2e8a10ec9a1b525380dd94c468a7b63545de
norm <normlorenz@gmail.com>
2018-07-03 14:23:39 -0700

    finished up before the forth of july

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,14 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
-  template: `
-  <div class='app'>
-  {{title}}
-  </div>
-  `
+  templateUrl: 'app.component.html'
 })
+
 export class AppComponent {
   title: string;
+  name: string = 'norm';
+  isHappy: boolean = false;
+  numberOne: number = 1;
+  numberTwo: number = 2;
+  logo: string = 'img/logo.svg';
   constructor() {
     this.title = 'ultimate angular';
   }
